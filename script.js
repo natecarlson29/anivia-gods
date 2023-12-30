6aec8f9a3bbc91b839bb09f75954e106b7ebdada
natecarlson29 <69022559+natecarlson29@users.noreply.github.com>
2023-12-30 17:36:40 -0600

    css

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ function expandBlob(e) {
     }
 }
 
-// function profPicError(e) {
-//     e.src = 'img/profileIcon/0.png';
-// }
+function profPicError(e) {
+    e.src = 'img/profileicon/0.png';
+}
 
 function toggleItems() {
     var timelineElement = document.getElementById('timeline-row');
@@ -319,14 +319,14 @@ function createPlayerList(summoners) {
         summonerListHtml += '<svg class="side-arrow" fill="#000000" width="64px" height="64px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M15.146 12.354l-5.792 5.792a.5.5 0 01-.854-.353V6.207a.5.5 0 01.854-.353l5.792 5.792a.5.5 0 010 .708z"/></svg>';
 
         /* Profile Image */
-        summonerListHtml += '<img class="med-img" src="img/profileicon/' + summoner.profileIconId + '.png"/>';
+        summonerListHtml += '<img onerror=profPicError(this) class="med-img" src="img/profileicon/' + summoner.profileIconId + '.png"/>';
 
         /* MatchId */
         // summonerListHtml += '<p class="bottom-right-text">' + summoner.matchId + '</p>';
 
         /* Summoner Name */
         summonerListHtml += '<div class="flex-column"><div class="summoner-name">' + summoner.summonerName + '<span class="riot-tag">#' + summoner.riotTag + '</span></div>';
-        summonerListHtml += '<div class="summoner-region">' + summoner.region + '</div></div></div>';
+        summonerListHtml += '<div class="summoner-region">' + summoner.region + '<span class="wins">' + summoner.wins + '</span> - <span class="losses">' + summoner.losses + '</span></div></div></div>';
 
         /* Rank */
         summonerListHtml += '<div class="tier"><img class="large-img" src="img/tier/' + summoner.tier + '.png"/>' + summoner.leaguePoints + ' LP</div>';
