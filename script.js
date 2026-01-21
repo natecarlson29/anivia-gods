bbbcf3ddeb2438c00c3e52e4ba2934c8dc38116e
natecarlson29 <69022559+natecarlson29@users.noreply.github.com>
2026-01-21 15:35:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -65,11 +65,11 @@ function getCDNImageUrl(path) {
             return `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/perk-images/${runeSubPath}`;
         }
 
-        // Transform summoner spell paths: /lol-game-data/assets/.../Spells/Icons2D/... -> /plugins/rcp-be-lol-game-data/global/default/v1/summoner-spells/...
+        // Transform summoner spell paths: /lol-game-data/assets/.../Spells/Icons2D/... -> /lol-game-data/assets/v1/summoner-spells/...
         if (path.includes('/Spells/Icons2D/')) {
-            // Use original casing from iconPath; CDragon spell files are case-sensitive
+            // Summoner spell images are in /lol-game-data/assets/v1/summoner-spells/
             const spellFileName = path.split('/').pop();
-            return `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/summoner-spells/${spellFileName}`;
+            return `https://raw.communitydragon.org/latest/lol-game-data/assets/v1/summoner-spells/${spellFileName}`;
         }
 
         // Default: append path as-is
@@ -85,7 +85,7 @@ function getCDNImageUrl(path) {
 
     if (path.startsWith('img/lanes/')) {
         const role = path.replace('img/lanes/', '').replace('.png', '');
-        // Normalize role names to CDragon position assets
+        // Normalize role names to Data Dragon position naming
         const normalizedRole = role.toLowerCase();
         const roleMap = {
             top: 'top',
@@ -103,8 +103,8 @@ function getCDNImageUrl(path) {
             utility: 'support'
         };
         const mappedRole = roleMap[normalizedRole] || normalizedRole;
-        // CDragon static assets for lane icons
-        return `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-static-assets/global/default/images/position/position_${mappedRole}.png`;
+        // Use Data Dragon position icons (reliable source)
+        return `https://ddragon.leagueoflegends.com/cdn/14.1.1/img/position/${mappedRole}.png`;
     }
 
     if (path.startsWith('img/tier/')) {
